5709048339c09507359136cde94bccaa3649668b
Ben Merckx <ben@codeurs.be>
2016-05-15 18:31:49 +0200

    Before loops

--- await/Await.hx
+++ await/Await.hx
@@ -2,201 +2,273 @@ package await;
 
 import haxe.macro.Context;
 import haxe.macro.Expr;
+import await.MacroTools.*;
 
 using tink.CoreApi;
 using tink.MacroApi;
+using Lambda;
 
 typedef AsyncContext = {
-	catcher: Null<String>,
-	transformed: Bool
+	?catcher: String,
+	loop: Bool,
+	needsResult: Bool
 }
 
 class AsyncField {
 	
 	var expr: Expr;
-	var count = 0;
-	var triggerName: String;
-	var triggerVar: Expr;
-	var catches: Array<Expr> = [];
 	
-	public function new(expr: Expr) {
+	public function new(expr: Expr)
 		this.expr = expr;
-		triggerName = tmp();
-		triggerVar = (macro $i{triggerName});
-	}
+	
+	public function transform(): Expr
+		return macro @:pos(expr.pos)
+			return tink.core.Future.async(function(__return) 
+				try ${process(expr, {loop: false, needsResult: false}, function(e) return e)}
+				catch(e: Dynamic) ${catchCall(null)}
+			)
+		;
 		
-	function getAwait(e: Expr): Null<Pair<Expr, Expr>>
-		switch e.expr {
-			case EMeta(m, em) if (m.name == ':await'):
-				return new Pair(e, em);
-			default:
-				var await = null;
-				e.iter(function(e) {
-					var a = getAwait(e);
-					if (a != null) await = a;
+	function catchCall(catcher: Null<String>)
+		return 
+			if (catcher == null) macro __return(tink.core.Outcome.Failure(e))
+			else catcher.resolve().call(['e'.resolve()]);
+		
+	function handler(tmp: String, ctx: AsyncContext, next: Expr -> Expr): Expr {
+		var body = unpack(next(macro await.FutureTools.getValue(${tmp.resolve()})));
+		body = macro try $body catch(e: Dynamic) ${catchCall(ctx.catcher)};
+		return body.func([tmp.toArg()], false).asExpr();
+	}
+	
+	function transformObj<T: {expr: Expr}>(ol: Array<T>, ctx: AsyncContext, final: Array<T> -> Expr): Expr {
+		var el = ol.map(function(v) return v.expr);
+		return transformList(el, ctx, function(transformedEl: Array<Expr>){
+			return final({
+				var i = 0;
+				ol.map(function(v) {
+					var obj = Reflect.copy(v);
+					obj.expr = transformedEl[i++];
+					return obj;
 				});
-				return await;
+			});
+		});
+	}
+	
+	function transformList(el: Array<Expr>, ctx: AsyncContext, final: Array<Expr> -> Expr): Expr {
+		function transformNext(i: Int, transformedEl: Array<Expr>): Expr {
+			if (i == el.length)
+				return final(transformedEl);
+			return process(el[i], ctx, function(transformed: Expr): Expr {
+				transformedEl.push(transformed);
+				return transformNext(i + 1, transformedEl);
+			});
 		}
-	
-	public function transform(): Expr {
-		var list = [], processed = process([expr], {catcher: null, transformed: false});
 		
-		return macro @:pos(expr.pos) {
-			return tink.core.Future.async(function($triggerName)
-				$b{catches.concat([processed])}
-			);
-		};
+		return transformNext(0, []);
+	}
 		
-		/*list.push(macro var $triggerName = tink.core.Future.trigger());
-		list = list.concat(catches);
-		switch processed.expr {
-			case EBlock(el): list = list.concat(el);
-			default: list.push(processed);
+	function process(e: Expr, ctx: AsyncContext, next: Expr -> Expr): Expr {
+		ctx = Reflect.copy(ctx);
+		switch e.expr {
+			case EBlock (el):
+				if (el.length == 0) return emptyExpr();
+				function line(i:Int): Expr {
+					if (i == el.length - 1)
+						return process(el[i], ctx, next);
+					
+					return process(el[i], ctx, function(transformed: Expr) {
+						var response = [transformed];
+						response.push(line(i+1));
+						return bundle(response);
+				  });
+				}
+				return line(0);
+			case EMeta (m, em) if (m.name == ':await'):
+				var tmp = tmpVar();
+				return process(em, ctx, function(transformed)
+					return macro @:pos(em.pos)
+						$transformed.handle(${handler(tmp, ctx, next)})
+				);
+			
+			
+			
+			case EFor (it, expr):
+			case EWhile (econd, e1, normalWhile):
+			case EBreak:
+			case EContinue:
+			
+			case ETry (e1, catches):
+				var wrapper = new AsyncWrapper(ctx, next);
+				var wrapperDeclaration = wrapper.declaration();
+				var name = tmpVar();
+				var transformedCatches = [
+					for (c in catches)
+						{type: c.type, name: c.name, expr: c.expr == null ? null : process(c.expr, ctx, wrapper.invocation)}
+				];
+				var body = ETry(macro throw e, transformedCatches).at(e.pos);
+				var func = body.func(['e'.toArg()]);
+				var declaration = EFunction(name, func).at(e.pos);
+				ctx.catcher = name;
+				var call = name.resolve().call(['e'.resolve()]);
+				//trace(e1.toString());
+				var entry = process(e1, ctx, wrapper.invocation);
+				entry = macro @:pos(e.pos)
+					try $entry catch(e: Dynamic) $call;
+				return bundle([wrapperDeclaration, declaration, entry]);
+			case EReturn (e1):
+				return process(e1, ctx, function(transformed)
+					return macro @:pos(e.pos)
+						{__return(tink.core.Outcome.Success($transformed)); return;}
+				);
+			case EThrow (e1):
+				if (ctx.catcher != null)
+					return process(e1, ctx, function(transformed)
+						return macro @:pos(e.pos)
+							{${ctx.catcher.resolve()}($transformed); return;}
+					);
+				return process(e1, ctx, function(transformed)
+					return macro @:pos(e.pos)
+						{__return(tink.core.Outcome.Failure($transformed)); return;}
+				);
+			case ETernary (econd, eif, eelse) |
+				 EIf (econd, eif, eelse):
+				var wrapper = new AsyncWrapper(ctx, next);
+				return process(econd, ctx, function(transformed) {
+					var declaration = wrapper.declaration();
+					var entry = EIf(
+						transformed,
+						process(eif, ctx, wrapper.invocation),
+						eelse == null ? wrapper.invocation(emptyExpr()) : process(eelse, ctx, wrapper.invocation)
+					).at(e.pos);
+					return bundle([declaration, entry]);
+				});
+			case ESwitch(e1, cases, edef):
+				var wrapper = new AsyncWrapper(ctx, next);
+				return process(e1, ctx, function(transformed) {
+					var transformedCases = [
+						for (c in cases)
+							if (c.expr == null) {expr: wrapper.invocation(emptyExpr()), guard: c.guard, values: c.values}
+							else {expr: process(c.expr, ctx, wrapper.invocation), guard: c.guard, values: c.values}
+					];
+					var transformedDefault = switch edef {
+						case null: emptyExpr();
+						case def if (def.expr == null): wrapper.invocation(emptyExpr());
+						default: process(edef, ctx, wrapper.invocation);
+					}
+					var declaration = wrapper.declaration();
+					var entry = ESwitch(transformed, transformedCases, transformedDefault).at(e.pos);
+					return bundle([declaration, entry]);
+				});
+			case EObjectDecl(obj):
+				ctx.needsResult = true;
+				return transformObj(obj, ctx, function(transformedObjs)
+					return next(EObjectDecl(transformedObjs).at(e.pos))
+				);
+			case EVars(obj):
+				return transformObj(obj, ctx, function(transformedObjs)
+					return next(EVars(transformedObjs).at(e.pos))
+				);
+			case EUntyped(e1):
+				return process(e1, ctx, function(transformed)
+					return next(EUntyped(transformed).at(e.pos))
+				);
+			case ECast(e1, t):
+				ctx.needsResult = true;
+				return process(e1, ctx, function(transformed)
+					return next(ECast(transformed, t).at(e.pos))
+				);
+			case EBinop(op, e1, e2):
+				ctx.needsResult = true;
+				return process(e1, ctx, function(t1)
+					return process(e2, ctx, function(t2)
+						return next(EBinop(op, t1, t2).at(e.pos))
+					)
+				);
+			case EArray(e1, e2):
+				ctx.needsResult = true;
+				return process(e1, ctx, function(t1)
+					return process(e2, ctx, function(t2)
+						return next(EArray(t1, t2).at(e.pos))
+					)
+				);
+			case EUnop(op, postFix, e1):
+				ctx.needsResult = true;
+				return process(e1, ctx, function(transformed)
+					return next(EUnop(op, postFix, transformed).at(e.pos))
+				);
+			case EField(e1, field):
+				ctx.needsResult = true;
+				return process(e1, ctx, function(transformed)
+					return next(EField(transformed, field).at(e.pos))
+				);
+			case ECheckType(e1, t):
+				ctx.needsResult = true;
+				return process(e1, ctx, function(transformed)
+					return next(ECheckType(transformed, t).at(e.pos))
+				);
+			case EArrayDecl(params):
+				ctx.needsResult = true;
+				return transformList(params, ctx, function(transformedParameters: Array<Expr>)
+					return next(EArrayDecl(transformedParameters).at(e.pos))
+				);
+			case ECall(e1, params):
+				ctx.needsResult = true;
+				return transformList(params, ctx, function(transformedParameters: Array<Expr>)
+					return process(e1, ctx, function(transformed) 
+						return next(ECall(transformed, transformedParameters).at(e.pos))
+					)
+				);
+			case ENew(t, params):
+				ctx.needsResult = true;
+				return transformList(params, ctx, function(transformedParameters: Array<Expr>)
+					return next(ENew(t, transformedParameters).at(e.pos))
+				);
+			default:
 		}
-		list.push(macro return $triggerVar.asFuture());*/
-		
-		
-		return list.toBlock(expr.pos);
+		return unpack(next(e));
 	}
 	
-	function tmp(): String return "__t"+(count++);
+}
+
+class AsyncWrapper {
 	
-	function copy(e: Expr): Expr
-		return {expr: e.expr, pos: e.pos};
-		
-	function catchCall(catcher: Null<String>): Expr {
-		if (catcher == null)
-			return macro $triggerVar(tink.core.Outcome.Failure(e));
-		return catcher.resolve().call(['e'.resolve()]);
+	var ctx: AsyncContext;
+	var next: Expr -> Expr;
+	var functionName: String;
+	var argName: String;
+	var empty: Bool = false;
+	
+	public function new(ctx: AsyncContext, next: Expr -> Expr) {
+		this.next = next;
+		this.ctx = ctx;
+		functionName = tmpVar();
+		argName = tmpVar();
 	}
 	
-	function extract(condition: Expr, expr: Expr): Expr {
-		var value = tmp();
-		var handle = copy(condition);
-		condition.expr = (macro $i{value}).expr;
-		return @:pos(condition.pos) macro {
-			var $value = $handle;
-			$expr;
-		};
-	}
-	
-	function context(ctx: AsyncContext) {
-		return {catcher: ctx.catcher, transformed: false};
-	}
-	
-	function addContinue(e: Expr) {
-		if (e == null) return;
-		e.expr = (macro {
-			${copy(e)}; __continue();
-		}).expr;
-	}
-	
-	function makeContinue(body: Expr, el: Array<Expr>, ctx: AsyncContext) {
-		var c = context(ctx);
-		var cb = body.func(['__continue'.toArg()], null, null, false).asExpr();
-		var next = process(el, c);
-		var nextAsCb = next.func([], null, null, false).asExpr();
-		return macro ($cb)($nextAsCb);
-	}
-	
-	function process(el: Array<Expr>, ctx: AsyncContext) {
-		var output: Array<Expr> = [];
-		
-		while (el.length > 0) {
-			var e = el.shift();
-			if (e == null) continue;
-			switch e.expr {
-				case EBlock(l):
-					el = l.concat(el);
-				case EIf(condition, e1, e2):
-					var await = getAwait(condition);
-					if (await != null) {
-						el.unshift(extract(condition, e));
-						output.push(process(el, ctx));
-						break;
-					} else {
-						var body: Expr = {expr: EIf(condition, process([e1], ctx), e2 == null ? null : process([e2], ctx)), pos: e.pos};
-						if (ctx.transformed) {
-							// change to yield
-							[e1, e2].map(addContinue);
-							//ctx.transformed = false;
-							output.push(makeContinue(body, el, ctx));
-							break;
-						} else {
-							output.push(body);
-						}
-					}
-				case ESwitch(condition, cl, edef):
-					var await = getAwait(condition);
-					if (await != null) {
-						el.unshift(extract(condition, e));
-						output.push(process(el, ctx));
-						break;
-					} else {
-						for (c in cl) {
-							c.expr.expr = process([c.expr], ctx).expr;
-						}
-						if (edef != null) 
-							edef = process([edef], ctx);
-						if (ctx.transformed) {
-							// change to yield
-							cl.map(function(c) return c.expr).concat([edef]).map(addContinue);
-							//ctx.transformed = false;
-							output.push(makeContinue(e, el, ctx));
-							break;
-						} else {
-							output.push(e);
-						}
-					}
-				case EReturn(e1):
-					e.expr = (macro $triggerVar(tink.core.Outcome.Success($e1))).expr;
-					output.push(macro @:pos(e.pos) {
-						${process([e], ctx)}; return;
-					});
-				case EThrow(e1):
-					if (ctx.catcher != null)
-						e.expr = ctx.catcher.resolve().call([e1]).expr;
-					else
-					e.expr = (macro $triggerVar(tink.core.Outcome.Failure($e1))).expr;
-					output.push(macro @:pos(e.pos) {
-						${process([e], ctx)}; return;
-					});
-				case ETry(e1, cl):
-					var name = tmp();
-					for (c in cl)
-						c.expr.expr = process([c.expr], {catcher: null, transformed: false}).expr;
-					var body = {pos: e.pos, expr: ETry(macro throw e, cl)};
-					var catchMethod = body.func(['e'.toArg()], null, null, false);
-					catches.push({expr: EFunction(name, catchMethod), pos: e.pos});
-					ctx.catcher = name;
-					e1.expr = process([e1], ctx).expr;
-					output.push(e);
+	public function declaration(): Expr {
+		var func;
+		if (ctx.needsResult) {
+			func = next(argName.resolve()).func([argName.toArg()], false);
+		} else {
+			var body = next(emptyExpr());
+			switch body.expr {
+				case EBlock([]): 
+					empty = true;
+					return emptyExpr();
 				default:
-					var await = getAwait(e);
-					if (await != null) {
-						var handle = copy(await.b);
-						var surprise = tmp(), value = tmp(), success = tmp();
-						await.a.expr = (macro cast $i{value}).expr;
-						el.unshift(e);
-						var body = process(el, ctx);
-						body = macro @:pos(e.pos)
-							try {
-								var $value = await.FutureTools.getValue($i{surprise});
-								$body;
-							} catch (e: Dynamic) ${catchCall(ctx.catcher)}
-						;
-						var func = body.func([surprise.toArg()], null, null, false).asExpr();
-						ctx.transformed = true;
-						output.push(process([macro @:pos(handle.pos) $handle.handle($func)], ctx));
-						break;
-					}
-					output.push(e);
 			}
+			func = body.func(false);
 		}
-		return switch output.length {
-			case 1: output[0];
-			default: output.toBlock(); 
-		}
+		
+		return EFunction(functionName, func).at();
+	}
+	
+	public function invocation(transformed: Expr): Expr {
+		if (empty)
+			return transformed;
+		if (ctx.needsResult)
+			return functionName.resolve().call([transformed]);
+		return bundle([transformed, functionName.resolve().call()]);
 	}
 	
 }
